d9d2870eed562fd958c58b809e70840a02d04e89
willroy <william@antroy.co.uk>
2017-12-27 22:12:22 +0000

    Platforms and collision

--- src/preload.js
+++ src/preload.js
@@ -10,7 +10,8 @@ preload.prototype = {
     this.game.load.image('start', 'assets/start.png');
     this.game.load.image('exit', 'assets/exit.png');
     this.game.load.image('character', 'assets/character.png');
-    
+    this.game.load.image('platform_ni', 'assets/platform_ni.png');
+    this.game.load.image('platform_i', 'assets/platform_i.png');
   },
   create: function(){
     this.game.state.start("GameTitle");
--- src/thegame.js
+++ src/thegame.js
@@ -1,19 +1,26 @@
 var theGame = function(game){}
 var character;
+var platform;
 theGame.prototype = {
   create: function(){
     this.stage.backgroundColor = "#FFFFFF"
     character = this.game.add.sprite(0,0,"character");
+    this.platforms = this.add.physicsGroup();
+    this.platforms.create(0,200,"platform_i");
+    this.platforms.setAll('body.allowGravity', false);
+    this.platforms.setAll('body.immovable', true);
     this.game.physics.enable(character);
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
     character.body.bounce.y = 0.2;
-    character.body.gravity.y = 300;
+    character.body.gravity.y = 700;
     character.body.collideWorldBounds = true;
     cursors = this.game.input.keyboard.createCursorKeys();
   },
   update: function(){
+    this.game.physics.arcade.collide(character, this.platforms);
+    var standing = character.body.blocked.down || character.body.touching.down;
     if (character.body) {
-      console.log("Velocity: " + character.body.velocity);
+      //console.log("Velocity: " + character.body.velocity);
       character.body.velocity.x = 0;
     }
     if (cursors.left.isDown) {
@@ -22,15 +29,9 @@ theGame.prototype = {
      if (cursors.right.isDown) {
       character.body.velocity.x = 200;
     } 
-    if (cursors.up.isDown) {
+    if (cursors.up.isDown && standing) {
       character.body.velocity.y = -200;
     }
-    //else {
-    //  character.body.velocity.x = 0;
-    //}
    
-  },
-  pressedRight: function(){
-    this.tweenRight(true);
- }
+  }
 }
