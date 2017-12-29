07e715c39d677ba7af9dde2988cd99b0e9043279
willroy <william@antroy.co.uk>
2017-12-29 14:36:39 +0000

    Turret

--- src/preload.js
+++ src/preload.js
@@ -6,12 +6,17 @@ preload.prototype = {
     var loadingBar = this.add.sprite(160,240,"loading");
     loadingBar.anchor.setTo(0.5,0.5);
     this.load.setPreloadSprite(loadingBar);
+    
     this.game.load.image('back', 'assets/background.png');
     this.game.load.image('start', 'assets/start.png');
     this.game.load.image('exit', 'assets/exit.png');
     this.game.load.image('character', 'assets/character.png');
-    this.game.load.image('platform_ni', 'assets/platform_ni.png');
+    this.game.load.image('platform_ni', 'assets/platformgrass.png');
     this.game.load.image('platform_i', 'assets/platform_i.png');
+    this.game.load.image('jump_pad', 'assets/jumppad.png');
+    this.game.load.image('turret', 'assets/turret.png');
+    this.game.load.image('rocket', 'assets/rocket.png');
+
   },
   create: function(){
     this.game.state.start("GameTitle");
--- src/thegame.js
+++ src/thegame.js
@@ -1,23 +1,37 @@
 var theGame = function(game){}
-var character;
-var platform;
+var character, rocket, reload;
 theGame.prototype = {
   create: function(){
     this.stage.backgroundColor = "#FFFFFF"
     character = this.game.add.sprite(0,0,"character");
-    this.platforms = this.add.physicsGroup();
-    this.platforms.create(0,200,"platform_i");
-    this.platforms.setAll('body.allowGravity', false);
-    this.platforms.setAll('body.immovable', true);
     this.game.physics.enable(character);
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
-    character.body.bounce.y = 0.2;
-    character.body.gravity.y = 700;
+    character.body.gravity.y = 1000;
     character.body.collideWorldBounds = true;
+  
+    this.turret = this.add.physicsGroup();
+    this.rocket = this.add.physicsGroup();
+    this.jump_pad = this.add.physicsGroup();
+    this.platforms = this.add.physicsGroup();
+    
+    this.platforms.create(0,300,"platform_ni");
+    this.jump_pad.create(200, 325, "jump_pad");
+    this.platforms.create(270,200,"platform_ni");
+    rocket = this.rocket.create(430, 325, "rocket"); 
+    this.turret.create(430, 325, "turret");
+    this.platforms.create(480,200,"platform_ni");
+    
+    this.platforms.setAll('body.allowGravity', false);
+    this.platforms.setAll('body.immovable', true);
+    this.turret.setAll('body.allowGravity', false);
+    this.turret.setAll('body.immovable', true);
     cursors = this.game.input.keyboard.createCursorKeys();
+    reload = 1;
   },
   update: function(){
     this.game.physics.arcade.collide(character, this.platforms);
+    this.game.physics.arcade.collide(character, this.turret);
+    var touchpad = this.game.physics.arcade.overlap(character, this.jump_pad);
     var standing = character.body.blocked.down || character.body.touching.down;
     if (character.body) {
       //console.log("Velocity: " + character.body.velocity);
@@ -30,8 +44,31 @@ theGame.prototype = {
       character.body.velocity.x = 200;
     } 
     if (cursors.up.isDown && standing) {
-      character.body.velocity.y = -200;
+      character.body.velocity.y = -400;
     }
-   
+    if (touchpad) {
+      character.body.velocity.y = -600;
+      
+    }
+    
+    reload += 1;
+    rocket.body.velocity.y = -400
+    if (reload == 70) {
+      rocket.kill(); 
+      rocket = this.rocket.create(430, 325, "rocket"); 
+      reload = 0;
+    }
   }
+
 }
+
+ //             _   _
+ //            /\\_//\
+ //           / o _ o \
+ //          /_   X   _\
+ //            \_____/
+ //            /  o  \
+ //           /       \__
+ //           \_(_|_)___ \
+ //                  (___/
+ 
